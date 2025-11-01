1dc85680d22b5270281a13a03efab03ccfea3f64
aluapana135-design <aluapana135@gmail.com>
2025-11-01 16:45:59 -0300

    Criar sdr_ui_false.c

---
+++ ui/sdr_ui_false.c
@@ -0,0 +1,4 @@
+void ui_draw_border(int x1, int y1, int x2, int y2)
+void ui_draw_window(const char* title, int x, int y, int w, int h)
+void ui_display_menu(const char** options, int count, int selected)
+void ui_clear_region(int x, int y, int w, int h)
