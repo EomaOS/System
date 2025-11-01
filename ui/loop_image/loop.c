02d03af26328c36a36c6852ea554fecbae8cf4af
aluapana135-design <aluapana135@gmail.com>
2025-11-01 16:40:53 -0300

    Criar ui/loop_image/loop.c

---
+++ ui/loop_image/loop.c
@@ -0,0 +1,4 @@
+void* get_framebuffer_address(void)
+bool init_graphics_mode(int width, int height, int bpp)
+bool scan_for_pattern(const unsigned char* screen_buffer, int screen_w, int screen_h, const unsigned char* pattern, int pattern_w, int pattern_h, int* found_x, int* found_y)
+void kernel_sleep_ms(unsigned int milliseconds)
