1d4777f4b8d944c1f0dc679103c8312fbc056194
aluapana135-design <aluapana135@gmail.com>
2025-11-01 16:48:01 -0300

    Atualizar o sdr_ui_false.c

--- ui/sdr_ui_false.c
+++ ui/sdr_ui_false.c
@@ -2,3 +2,9 @@ void ui_draw_border(int x1, int y1, int x2, int y2)
 void ui_draw_window(const char* title, int x, int y, int w, int h)
 void ui_display_menu(const char** options, int count, int selected)
 void ui_clear_region(int x, int y, int w, int h)
+/** input */
+int ui_get_key(void)
+void ui_read_command(char* buffer, int max_len)
+int ui_handle_menu_input(const char** options, int count)
+/** loop */
+void stdio_ui_start_shell(void)
